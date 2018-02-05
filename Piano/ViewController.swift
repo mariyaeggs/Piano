c79ba409f5f495d20ef9e03b34cda33cc33b819c
mariyaeggs <mariyawilson@live.com>
2018-02-05 14:11:48 -0800

    Setup initial project and files.

---
+++ Piano/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Piano
+//
+//  Created by Mariya Eggensperger  on 2/5/18.
+//  Copyright © 2018 Mariya Eggensperger. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
