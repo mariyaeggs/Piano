01d687bc3806f8df0d619b41d180622693893a80
mariyaeggs <mariyawilson@live.com>
2018-02-05 14:27:08 -0800

    Link piano buttons with sound in ViewController.swift.

--- Piano/ViewController.swift
+++ Piano/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate{
 
+   // Declare an audio player
+   var audioPlayer : AVAudioPlayer!
+   let regularNoteArray = ["c", "d", "e", "f", "g", "a", "b"]
+   let sharpNoteArray = [""]
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+   }
+   
+   @IBAction func regularNoteTapped(_ sender: UIButton) {
+      playSoundFile(selectedSoundFile: regularNoteArray[sender.tag - 1])
+   }
+   @IBAction func sharpNoteTapped(_sender: UIButton) {
+      
+   }
+   func playSoundFile(selectedSoundFile : String) {
+      let soundURL = Bundle.main.url(forResource: selectedSoundFile, withExtension: "wav")
+      do {
+         try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+      } catch {
+         print("Error in file handling.", error)
+      }
+      // Play audio
+      audioPlayer.play()
    }
 
    override func didReceiveMemoryWarning() {
